0fbb6280dc3e433fd7c3c8aff16e35d5c6b72075
jayoharedee <shme@protonmail.com>
2016-02-24 00:31:52 -0500

    adjusted AJAX call started callback for modal

--- srv/js/send.js
+++ srv/js/send.js
@@ -3,13 +3,19 @@
 function send() {
     var email = validate('email').emailField(),
 	    name = validate('name').nameField(),
-		text = validate('particulars').textField();
+		text = validate('particulars').textField(),
+        num = $("#number").val();
 
     if (!email || !name) { return false; }
 
-    $.post("/application/php/note.php", {
-        e: email,
-		n: name,
-		t: text
-	}); 
+    $.post("/mail/lead", {
+        email: email,
+		name: name,
+		particulars: text,
+        number: num
+	})
+    .success(function() {
+        alert('Thank you for your interest in Homestead Roofs. A representative will be contacting you shortly.')
+        // do modal triggering here with json return later
+    });
 }
